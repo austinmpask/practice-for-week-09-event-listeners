66bed8b8e3656393e082a9ca745f2dc007cd4b4d
austinmpask <austinmpask@gmail.com>
2024-01-31 13:14:03 -0800

    done

--- events.js
+++ events.js
@@ -1,1 +1,28 @@
-// Your code here
+document.addEventListener("DOMContentLoaded", (event) => {
+  console.log("dom loaded");
+
+  const input1 = document.getElementById("red-input");
+  input1.addEventListener("input", () => {
+    if (input1.value === "red") {
+      document.getElementById("section-1").style.backgroundColor = "red";
+    }
+  });
+
+  const addItemButton = document.getElementById("add-item");
+  addItemButton.addEventListener("click", () => {
+    const listAdd = document.getElementById("list-add");
+
+    if (listAdd.value) {
+      const newLi = document.createElement("li");
+      newLi.innerHTML = listAdd.value;
+      document.getElementById("section-2").children[1].appendChild(newLi);
+      listAdd.value = "";
+    }
+  });
+
+  const colorSelect = document.getElementById("color-select");
+  colorSelect.addEventListener("change", () => {
+    document.getElementById("section-3").style.backgroundColor =
+      colorSelect.value;
+  });
+});
